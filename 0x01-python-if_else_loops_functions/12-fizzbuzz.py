7e0b82c1805d40a95de5a23f582ef74582c5b99c
wassim <wassim.ouanaou@hotmail.fr>
2023-10-23 05:41:33 -0700

    W

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(101):
-        if i %% 3 == 0:
-            if i %% 5 == 0:
+        if i % 3 == 0:
+            if i % 5 == 0:
                 print("{}".format("FizzBuzz"), end=" ")
             else:
                 print("{}".format("Fizz"), end=" ")
-        elif i %% 5 == 0:
+        elif i % 5 == 0:
             print("{}".format("Buzz"), end=" ")
         else:
             print("{}".format(i), end=" ")
