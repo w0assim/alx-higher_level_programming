3daf5af1551cde96e4b917db297db54d06c2727d
wassim <wassim.ouanaou@hotmail.fr>
2023-10-24 07:49:46 -0700

    W

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+if __name__ = "__main__":
+    sum = 0
+    if len(argv) <= 1:
+        print("{}".format(sum))
+    else:
+        for i in range(1, len(argv)):
+            sum += argv[i]
