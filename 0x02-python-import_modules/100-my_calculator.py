c5cc1491316f7078f17946afb36a0a9de30ffc81
wassim <wassim.ouanaou@hotmail.fr>
2023-10-24 12:10:00 -0700

    W

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    match argv[2]:
+        case '+':
+            print("{} {} {} = {}".format(int(argv[1]), int(argv[2]), int(argv[3]), add(a, b)))
+        case '-':
+            print("{} {} {} = {}".format(int(argv[1]), int(argv[2]), int(argv[3]), sub(a, b)))
+        case '*':
+            print("{} {} {} = {}".format(int(argv[1]), int(argv[2]), int(argv[3]), mul(a, b)))
+        case '/':
+            print("{} {} {} = {}".format(int(argv[1]), int(argv[2]), int(argv[3]), div(a, b)))
+        default:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
