e21ff02ca7388c97f1abd6c236bdc0ada264573b
wassim <wassim.ouanaou@hotmail.fr>
2023-10-24 00:59:00 -0700

    w

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 from add_0.py import add
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ = "__main__":
+    exec()
+
+def exec():
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
