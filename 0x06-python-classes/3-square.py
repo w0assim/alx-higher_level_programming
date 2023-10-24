c9ea18f248e139d86300328bc23f379f8f821b30
wassim <wassim.ouanaou@hotmail.fr>
2023-10-24 16:58:42 -0700

    W

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,14 +4,19 @@
 
 
 class Square:
+
     """s"""
+
     def __init__(self, size=0):
+
         """s"""
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """s"""
-         return (self.__size * self.__size)
+        return (self.__size * self.__size)
