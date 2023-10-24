72baaaa9e7b8ff42cd410118a19a155089219954
wassim <wassim.ouanaou@hotmail.fr>
2023-10-24 07:08:10 -0700

    W

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) == 1:
+        print("{} arguments.".format(len(argv) - 1))
+    else:
+        if len(argv) == 2:
+            print("{} argument:".format(len(argv) - 1))
+        else:
+            print("{} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
