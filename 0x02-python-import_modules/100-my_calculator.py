c4bdc7ea780219884367c984111dd858346d85f4
wassim <wassim.ouanaou@hotmail.fr>
2023-10-24 12:18:32 -0700

    W

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,15 +6,14 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    match argv[2]:
-        case '+':
-            print("{} {} {} = {}".format(int(argv[1]), int(argv[2]), int(argv[3]), add(a, b)))
-        case '-':
-            print("{} {} {} = {}".format(int(argv[1]), int(argv[2]), int(argv[3]), sub(a, b)))
-        case '*':
-            print("{} {} {} = {}".format(int(argv[1]), int(argv[2]), int(argv[3]), mul(a, b)))
-        case '/':
-            print("{} {} {} = {}".format(int(argv[1]), int(argv[2]), int(argv[3]), div(a, b)))
-        default:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+    if argv[2] == '+':
+        print("{} {} {} = {}".format(int(argv[1]), int(argv[2]), int(argv[3]), add(a, b)))
+    elif argv[2] == '-':
+        print("{} {} {} = {}".format(int(argv[1]), int(argv[2]), int(argv[3]), sub(a, b)))
+    elif argv[2] == '*':
+        print("{} {} {} = {}".format(int(argv[1]), int(argv[2]), int(argv[3]), mul(a, b)))
+    elif argv[2] == '/':
+        print("{} {} {} = {}".format(int(argv[1]), int(argv[2]), int(argv[3]), div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
