6719468236cec0f40f3e2edb494142f7ab28154d
wassim <wassim.ouanaou@hotmail.fr>
2023-10-24 12:19:31 -0700

    W

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     if argv[2] == '+':
-        print("{} {} {} = {}".format(int(argv[1]), int(argv[2]), int(argv[3]), add(a, b)))
+        print("{} {} {} = {}".format(int(argv[1]), (argv[2]), int(argv[3]), add(a, b)))
     elif argv[2] == '-':
-        print("{} {} {} = {}".format(int(argv[1]), int(argv[2]), int(argv[3]), sub(a, b)))
+        print("{} {} {} = {}".format(int(argv[1]), (argv[2]), int(argv[3]), sub(a, b)))
     elif argv[2] == '*':
-        print("{} {} {} = {}".format(int(argv[1]), int(argv[2]), int(argv[3]), mul(a, b)))
+        print("{} {} {} = {}".format(int(argv[1]), (argv[2]), int(argv[3]), mul(a, b)))
     elif argv[2] == '/':
-        print("{} {} {} = {}".format(int(argv[1]), int(argv[2]), int(argv[3]), div(a, b)))
+        print("{} {} {} = {}".format(int(argv[1]), (argv[2]), int(argv[3]), div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
