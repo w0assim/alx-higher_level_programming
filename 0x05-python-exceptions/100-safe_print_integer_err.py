f2f4d872ad8673199ad6f08891742c6350fbb380
wassim <wassim.ouanaou@hotmail.fr>
2023-10-24 18:02:04 -0700

    W

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
-     try:
+    try:
         print("{:d}".format(value))
         return (True)
     except (TypeError, ValueError):
