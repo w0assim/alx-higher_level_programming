3eb387ee10da8e8e2bde0e6eb4d9321025679b3d
wassim <wassim.ouanaou@hotmail.fr>
2023-10-24 17:00:14 -0700

    W

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """s"""
+
+
 class Square:
     """s"""
     def __init__(self, size=0):
         """s"""
         self.size = size
+
     @property
     def size(self):
         """s"""
-         return (self.__size)
+        return (self.__size)
 
     @size.setter
     def size(self, value):
