76df4f9cc9ff7346dd1f923fc6e1946a927705e6
wassim <wassim.ouanaou@hotmail.fr>
2023-10-24 17:26:34 -0700

    W

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - Check cycle.
+ * @list: list
+ *
+ * Return: 0/1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	slow = list->next;
+	fast = list->next->next;
+
+	while (slow && fast && fast->next)
+	{
+		if (slow == fast)
+			return (1);
+
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+	return (0);
+}
